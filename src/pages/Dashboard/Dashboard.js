689e8504f5a3ed7e9437dd227b36e9ef824dec47
Emmanuel Ayodele <emmayodayo@gmail.com>
2022-09-13 13:37:25 +0100

    add styling

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import style from './Dashboard.module.css'
+import CTAButton from '../../components/CTAButton/CTAButton';
+import ExplanationBox from '../../containers/ExplanationBox/ExplanationBox';
 
 const Dashboard = () => {
 	return (
-		<div>
-			<span>dashboard</span>
+		<div className={style.Dashboard}>
+			<span>Your impact score is: Excellent</span>
+			<div>
+				dashboard progress bar
+			</div>
+			<div className={style.DashboardButtons}>
+				<CTAButton/>
+				<CTAButton/>
+			</div>
+
+			<ExplanationBox/>
 		</div>
 	)
 }
