928b221df73774f7902675ad450aa10bb597eed3
Emmanuel Ayodele <emmayodayo@gmail.com>
2022-09-13 12:17:01 +0100

    update styling

--- src/components/Explanation/Explanation.js
+++ src/components/Explanation/Explanation.js
@@ -8,14 +8,16 @@ const Explanation = () => {
 				<div className={style.NumberWrapper}>
 					<span className={style.NumberText}>Step 1</span>
 				</div>
-				
-				<span className={style.ExplanationText}>
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Est repellendus libero quaerat, voluptas
-				</span>
 
-				<span className={style.ExplanationDetails}>
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae nobis provident nulla mollitia neque
-				</span>
+				<div className={style.ExplanationTextWrapper}>
+					<span className={style.ExplanationText}>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Est repellendus libero quaerat, voluptas
+					</span>
+
+					<span className={style.ExplanationDetails}>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae nobis provident nulla mollitia neque
+					</span>
+				</div>
 			</div>
 		</div>
 	)
