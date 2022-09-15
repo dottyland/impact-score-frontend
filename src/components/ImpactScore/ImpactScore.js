f1d68a220343c64ab4c2c3daca416b1b642d80da
Emmanuel Ayodele <emmayodayo@gmail.com>
2022-09-15 15:25:56 +0100

    Add impact score component

---
+++ src/components/ImpactScore/ImpactScore.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import style from './ImpactScore.module.css';
+
+const ImpactScore = ({ value, maxValue }) => {
+  const val = (value / maxValue) * 100;
+  const deg = (180 / 100) * val;
+  return (
+    <div className={style.indicator}>
+      <span className={style.bar} style={{ transform: `rotate(${deg}deg)` }} />
+      <span className={style.result}>
+        <span>{value}</span>/<span>{maxValue}</span>
+      </span>
+    </div>
+  );
+}
+
+
+export default ImpactScore
--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -2,17 +2,17 @@ import React from 'react';
 import style from './Dashboard.module.css'
 import CTAButton from '../../components/CTAButton/CTAButton';
 import ExplanationBox from '../../containers/ExplanationBox/ExplanationBox';
+import ImpactScore from '../../components/ImpactScore/ImpactScore';
 
 const Dashboard = () => {
 	return (
 		<div className={style.Dashboard}>
 			<span>Your impact score is: Excellent</span>
-			<div>
-				dashboard progress bar
-			</div>
+			<ImpactScore value={86} maxValue={100}/>
+
 			<div className={style.DashboardButtons}>
-				<CTAButton/>
-				<CTAButton/>
+				<CTAButton buttonText = 'stuff' click = {() => 'hi'}/>
+				<CTAButton buttonText = 'stuff' click = {() => 'hi'}/>
 			</div>
 
 			<ExplanationBox/>
