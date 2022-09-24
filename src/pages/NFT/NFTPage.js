e554c919c669e4ba40b48feb785291bf58d034ec
Emmanuel Ayodele <emmayodayo@gmail.com>
2022-09-24 18:03:41 +0100

    Add UI fixes

--- src/pages/NFT/NFTPage.js
+++ src/pages/NFT/NFTPage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import style from './NFTPage.module.css';
-import NFTImage from '../../assets/NFT-test.png'
+import NFTImage from '../../assets/NFTImage.png'
 import CTAButtton from '../../components/CTAButton/CTAButton';
 import ExplanationBox from '../../containers/ExplanationBox/ExplanationBox';
 import { useAccount, useConnect, useDisconnect } from 'wagmi'
@@ -14,14 +14,12 @@ const NFTPage = () => {
 		<div className={style.NFTPage}>
 			<div className={style.NFTDetails}>
 				<img src={NFTImage} alt="" />
-				<span className={style.PageTitle}>Congratulations, you are #21!</span>
+				<span className={style.PageTitle}>Congratulations, you are Impact Self #21!</span>
 
 				<div className={style.ButtonsContainer}>
-					<Link to='/NFTs'>
-						<CTAButtton buttonText='Share on lenster' />
-					</Link>
+					<CTAButtton buttonText='Share on lenster' />
 
-					<a href="https://twitter.com/intent/tweet?text=I%20just%20claimed%20my%20Impact%20Self!%20Jealous?%20Join%20me%20in%20saving%20the%20🌍%20with%20Dottyland!">
+					<a href="https://twitter.com/intent/tweet?text=I%20just%20claimed%20my%20Impact%20Self!%20Jealous?%20Join%20me%20in%20saving%20the%20🌍%20with%20@dottyland_xyz!">
 						<CTAButtton buttonText='Share on Twitter' />
 					</a>
 
--- src/pages/Privacy/Privacy.js
+++ src/pages/Privacy/Privacy.js
@@ -5,6 +5,10 @@ import CTAButtton from '../../components/CTAButton/CTAButton';
 import { Link } from 'react-router-dom';
 import ExplanationBox from '../../containers/ExplanationBox/ExplanationBox';
 import PrivacyContent from '../../data/PrivacyContent';
+import lockIcon from '../../assets/lock.svg';
+import unlockIcon from '../../assets/unlock.svg';
+import twitterIcon from '../../assets/bi_twitter.svg';
+import shareIcon from '../../assets/share_lens.svg';
 
 
 const Privacy = () => {
@@ -19,24 +23,47 @@ const Privacy = () => {
 
 				<div className={style.ButtonsContainer}>
 					<Link to='/NFTs'>
-						<CTAButtton buttonText='Make impact self public' />
+						<CTAButtton
+							buttonIcon={unlockIcon}
+							buttonText='MAKE IMPACT SELF PUBLIC' />
 					</Link>
 
 					<Link to='/NFTs'>
-						<CTAButtton buttonText='Make impact self private' />
+						<CTAButtton
+							buttonIcon={lockIcon}
+							buttonText='MAKE IMPACT SELF PRIVATE' />
 					</Link>
 				</div>
 			</div>
-			<ExplanationBox
-				ExplanationBoxText='Why we think you should make it public '
+			{/* <ExplanationBox
+				ExplanationBoxText=''
 				ExplanationContent={PrivacyContent.explanationData}
-			/>
+			/> */}
 
-			<div className={style.ButtonsContainer}>
-				<a href="https://twitter.com/intent/tweet?text=I%20just%20claimed%20my%20Impact%20Self!%20Jealous?%20Join%20me%20in%20saving%20the%20🌍%20with%20Dottyland!">
-					<CTAButtton buttonText='Share on Twitter' />
-				</a>
-				<CTAButtton buttonText='Share on Lens' />
+			<div className={style.InfoBox}>
+				<span>
+					Why we think you should make it public
+				</span>
+
+				<span>
+					Amplify this message to help halt the climate crisis!
+				</span>
+
+				<span>
+					We understand that not everyone is comfortable sharing their actions. We believe that the more we make our climate-positive actions part of who we are, and make it part of the conversation, the more people will join the movement and take action.
+				</span>
+
+				<div className={style.ButtonsContainer}>
+					<a href="https://twitter.com/intent/tweet?text=I%20just%20claimed%20my%20Impact%20Self!%20Jealous?%20Join%20me%20in%20saving%20the%20🌍%20with%20@dottyland_xyz!">
+						<CTAButtton
+							buttonIcon={twitterIcon}
+							buttonText='Share on Twitter' />
+					</a>
+					<CTAButtton
+						buttonIcon={shareIcon}
+						buttonText='Share on Lens'
+					/>
+				</div>
 			</div>
 		</div>
 	)
