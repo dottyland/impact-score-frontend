e4c6027da267fe623f602d128cb9e4e773418c40
Emmanuel Ayodele <emmayodayo@gmail.com>
2022-09-24 20:23:22 +0100

    Add updates

--- src/pages/NFT/NFTPage.js
+++ src/pages/NFT/NFTPage.js
@@ -6,31 +6,51 @@ import ExplanationBox from '../../containers/ExplanationBox/ExplanationBox';
 import { useAccount, useConnect, useDisconnect } from 'wagmi'
 import { Link } from 'react-router-dom';
 import NFTContent from '../../data/NFTContent';
+import twitterIcon from '../../assets/bi_twitter.svg';
+import shareIcon from '../../assets/share_lens.svg';
+import eyeIcon from '../../assets/eyeIcon.svg';
+
+
 
 const NFTPage = () => {
 	const { address, isConnected } = useAccount();
-	
+
 	return (
 		<div className={style.NFTPage}>
 			<div className={style.NFTDetails}>
-				<img src={NFTImage} alt="" className={style.NFTImage}/>
+				<img src={NFTImage} alt="" className={style.NFTImage} />
 				<span className={style.PageTitle}>Congratulations, you are Impact Self #21!</span>
 
 				<div className={style.ButtonsContainer}>
-					<CTAButtton buttonText='Share on lenster' />
+					<CTAButtton
+						buttonIcon={shareIcon}
+						buttonText='Share on lenster' />
 
 					<a href="https://twitter.com/intent/tweet?text=I%20just%20claimed%20my%20Impact%20Self!%20Jealous?%20Join%20me%20in%20saving%20the%20🌍%20with%20@dottyland_xyz!">
-						<CTAButtton buttonText='Share on Twitter' />
+						<CTAButtton
+							buttonIcon={twitterIcon}
+							buttonText='Share on Twitter' />
 					</a>
 
 				</div>
 			</div>
-			<ExplanationBox
-				ExplanationBoxText='Why share widely?'
-				ExplanationContent={NFTContent.explanationData}
-			/>
+			
+
+			<div className={style.InfoBox}>
+				<div className={style.TextContainer}>
+					<span>Why share widely?</span>
+
+					<span>The more we make our environmental impact a part of who we are, and part of the conversation, the more people will join the movement and take action. </span>
+
+					<span>
+						The wider the network of Impact Selves, the larger its impact and higher its value.
+					</span>
+				</div>
+			</div>
 			<Link to='/NFTs'>
-				<CTAButtton buttonText='View impact selves' />
+				<CTAButtton
+					buttonIcon={eyeIcon}
+					buttonText='View impact selves' />
 			</Link>
 		</div>
 	)
--- src/pages/Privacy/Privacy.js
+++ src/pages/Privacy/Privacy.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import style from './Privacy.module.css';
-import NFTImage from '../../assets/NFT-test.png'
+import NFTImage from '../../assets/NFTImage.png'
 import CTAButtton from '../../components/CTAButton/CTAButton';
 import { Link } from 'react-router-dom';
 import ExplanationBox from '../../containers/ExplanationBox/ExplanationBox';
@@ -24,10 +24,22 @@ const Privacy = () => {
 				{PrivacyContent.pageTitle}
 			</span>
 
+			<div className={style.PrivacyInfo}>
+				<span>
+					Public: Everyone can see your Impact Self + your impact score.
+				</span>
+
+				<span>
+					Private: People can see you have an Impact Self, but not your score or level.
+				</span>
+
+			</div>
+
 			<div className={style.PrivacyDetails}>
 				{
 					isPrivate === false ?
-						<img src={NFTImage} alt="" /> : 'Your impact self is Private'
+						<img src={NFTImage} alt="" className={style.NFTImage} />
+						: 'Your impact self is Private'
 
 				}
 
@@ -49,17 +61,19 @@ const Privacy = () => {
 			/> */}
 
 			<div className={style.InfoBox}>
-				<span>
-					Why we think you should make it public
-				</span>
+				<div className={style.TextContainer}>
+					<span>
+						Why we think you should make it public
+					</span>
 
-				<span>
-					Amplify this message to help halt the climate crisis!
-				</span>
+					<span>
+						Amplify this message to help halt the climate crisis!
+					</span>
 
-				<span>
-					We understand that not everyone is comfortable sharing their actions. We believe that the more we make our climate-positive actions part of who we are, and make it part of the conversation, the more people will join the movement and take action.
-				</span>
+					<span>
+						We understand that not everyone is comfortable sharing their actions. We believe that the more we make our climate-positive actions part of who we are, and make it part of the conversation, the more people will join the movement and take action.
+					</span>
+				</div>
 
 				<div className={style.ButtonsContainer}>
 					<a href="https://twitter.com/intent/tweet?text=I%20just%20claimed%20my%20Impact%20Self!%20Jealous?%20Join%20me%20in%20saving%20the%20🌍%20with%20@dottyland_xyz!">
