084577cae46b937d938eef670a5e336f9b1a928a
dhruv035 <dhruv035@gmail.com>
2022-09-25 13:16:59 +0530

    API3

--- src/components/ImpactScore/ImpactScore.js
+++ src/components/ImpactScore/ImpactScore.js
@@ -5,7 +5,7 @@ import style from './ImpactScore.module.css';
 import axios from "axios";
 import Spinner from '../Spinner/Spinner';
 
-const API_URL = 'https://impact-api.vercel.app'
+const API_URL = 'https://impact-api-bepw.vercel.app'
 const ImpactScore = ({ value, maxValue }) => {
 	const { address, isConnected } = useAccount()
 	const [score, setScore] = useState(0);
