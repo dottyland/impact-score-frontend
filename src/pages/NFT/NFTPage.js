99e49f0d80a46a7ab243d9a1aa29c4f06fe61aa0
dhruv035 <dhruv035@gmail.com>
2022-09-25 10:49:10 +0530

    New8

--- src/pages/NFT/NFTPage.js
+++ src/pages/NFT/NFTPage.js
@@ -96,8 +96,88 @@ const NFTPage = () => {
 	  })
 	  console.log('Lens example data: ', response,login)
 	  const qProfile=`query DefaultProfile {
-		defaultProfile(request: { ethereumAddress: "0x3A5bd1E37b099aE3386D13947b6a90d97675e5e3"}) {
-		  id}`
+		defaultProfile(request: { ethereumAddress: "${address}"}) {
+		  id
+		  name
+		  bio
+		  isDefault
+		  attributes {
+			displayType
+			traitType
+			key
+			value
+		  }
+		  followNftAddress
+		  metadata
+		  handle
+		  picture {
+			... on NftImage {
+			  contractAddress
+			  tokenId
+			  uri
+			  chainId
+			  verified
+			}
+			... on MediaSet {
+			  original {
+				url
+				mimeType
+			  }
+			}
+		  }
+		  coverPicture {
+			... on NftImage {
+			  contractAddress
+			  tokenId
+			  uri
+			  chainId
+			  verified
+			}
+			... on MediaSet {
+			  original {
+				url
+				mimeType
+			  }
+			}
+		  }
+		  ownedBy
+		  dispatcher {
+			address
+			canUseRelay
+		  }
+		  stats {
+			totalFollowers
+			totalFollowing
+			totalPosts
+			totalComments
+			totalMirrors
+			totalPublications
+			totalCollects
+		  }
+		  followModule {
+			... on FeeFollowModuleSettings {
+			  type
+			  contractAddress
+			  amount {
+				asset {
+				  name
+				  symbol
+				  decimals
+				  address
+				}
+				value
+			  }
+			  recipient
+			}
+			... on ProfileFollowModuleSettings {
+			 type
+			}
+			... on RevertFollowModuleSettings {
+			 type
+			}
+		  }
+		}
+	  }`
 		  const fetchProfile=await apolloClient.query({
 			query:gql(qProfile),
 		  })
